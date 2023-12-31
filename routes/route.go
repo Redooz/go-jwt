f76afb354cc05d7d7679117f3b336172c4dd8212
Nicolás Olmos <yohannicolas2003@gmail.com>
2023-12-30 20:54:02 -0500

    feat: redirecting from root path to swagger docs

--- routes/route.go
+++ routes/route.go
@@ -1,16 +1,16 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitRoutes() *gin.Engine {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Pong",
-		})
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "/swagger/index.html")
 	})
 
 	InitAuthRoutes(r)
